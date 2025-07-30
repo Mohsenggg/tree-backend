f94de6b2ed6888c83e0e3ee2e795caeb44899e68
Mohsenggg <mohse.gamal@gmail.com>
2025-07-30 13:07:44 +0300

    start spring project

---
+++ src/main/java/com/mgh/backend/TreeProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mgh.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgh/backend/TreeProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgh.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
